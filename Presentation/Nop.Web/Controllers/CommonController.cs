c7e912176e44222b250b60c92f1aaed46052347b
vacvin <vacvin@users.noreply.github.com>
2015-12-17 13:29:20 +0800

    fIx

--- Presentation/Nop.Web/Controllers/CommonController.cs
+++ Presentation/Nop.Web/Controllers/CommonController.cs
@@ -1,4 +1,46 @@
-
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Nop.Core;
+using Nop.Core.Caching;
+using Nop.Core.Domain;
+using Nop.Core.Domain.Blogs;
+using Nop.Core.Domain.Catalog;
+using Nop.Core.Domain.Common;
+using Nop.Core.Domain.Customers;
+using Nop.Core.Domain.Forums;
+using Nop.Core.Domain.Localization;
+using Nop.Core.Domain.Messages;
+using Nop.Core.Domain.News;
+using Nop.Core.Domain.Orders;
+using Nop.Core.Domain.Tax;
+using Nop.Core.Domain.Vendors;
+using Nop.Services.Catalog;
+using Nop.Services.Common;
+using Nop.Services.Customers;
+using Nop.Services.Directory;
+using Nop.Services.Forums;
+using Nop.Services.Localization;
+using Nop.Services.Logging;
+using Nop.Services.Messages;
+using Nop.Services.Orders;
+using Nop.Services.Security;
+using Nop.Services.Seo;
+using Nop.Services.Topics;
+using Nop.Services.Vendors;
+using Nop.Web.Extensions;
+using Nop.Web.Framework;
+using Nop.Web.Framework.Localization;
+using Nop.Web.Framework.Security;
+using Nop.Web.Framework.Security.Captcha;
+using Nop.Web.Framework.Themes;
+using Nop.Web.Infrastructure.Cache;
+using Nop.Web.Models.Catalog;
+using Nop.Web.Models.Common;
+using Nop.Web.Models.Topics;
 
 namespace Nop.Web.Controllers
 {
